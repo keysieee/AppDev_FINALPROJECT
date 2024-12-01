9b0993a455e4c28d5e36ea8a5f36751fb3b77b79
Mary Joy Macalindol <maryjoymacalindol1209@gmail.com>
2024-12-01 14:30:56 +0800

    Delete RJ_Consumer_Goods_Trading/routes/admin directory

--- RJ_Consumer_Goods_Trading/routes/admin/inoutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inOutController = require('../controller/admin/inOutController');  // Correct path
-
-// Ensure the controller method exists
-router.get('/', inOutController.index);  // Correct usage of inOutController.index
-
-module.exports = router;
--- RJ_Consumer_Goods_Trading/routes/admin/taskRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tasksController = require('../controller/tasksController');
-
-router.get('/', tasksController.getTasks);
-router.post('/add', tasksController.addTask);
-
-module.exports = router;

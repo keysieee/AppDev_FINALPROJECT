af71086f399d0f2220b855423b1b90ce3ac59369
Mary Joy Macalindol <maryjoymacalindol1209@gmail.com>
2024-12-01 14:27:09 +0800

    Update adminController.js

--- RJ_Consumer_Goods_Trading/controller/adminController.js
+++ RJ_Consumer_Goods_Trading/controller/adminController.js
@@ -1,9 +1,23 @@
-exports.getAdminPage = (req, res) => {
-    if (req.session && req.session.role === 'admin') {
-        res.render('admin', {
-            role: req.session.role, // Pass role to the EJS template
-        });
-    } else {
-        res.redirect('/login'); // Redirect to login if not authorized
-    }
+exports.dashboard = (req, res) => {
+    res.render('admin/dashboard', { title: 'Dashboard' });
+};
+
+exports.tasks = (req, res) => {
+    res.render('admin/tasks', { title: 'Tasks' });
+};
+
+exports.inout = (req, res) => {
+    res.render('admin/inout', { title: 'In/Out' });
+};
+
+exports.services = (req, res) => {
+    res.render('admin/services', { title: 'Services' });
+};
+
+exports.inventory = (req, res) => {
+    res.render('admin/inventory', { title: 'Inventory' });
+};
+
+exports.shop = (req, res) => {
+    res.render('admin/shop', { title: 'Shop' });
 };

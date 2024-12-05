f8c79e901fa02622acd3310f016d8c4b19e27212
Keysie Dela Cruz <161782851+keysieee@users.noreply.github.com>
2024-12-05 15:03:49 +0800

    Delete RJ_Consumer_Goods_Trading/routes/adminEmployeeInfo.js

--- RJ_Consumer_Goods_Trading/routes/adminEmployeeInfo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controller/adminEmployeeInfoController');
-
-// Admin can view all employee information
-router.get('/admin/employeeInfo', adminController.getAllEmployeeInfoPage);
-
-module.exports = router;
